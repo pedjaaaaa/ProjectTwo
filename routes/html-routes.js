cae57e8cfbc73450afd90f8835a1b8e99229cc89
shine7653 <shine@gmail.com>
2020-01-09 19:20:43 -0800

    little of edit

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,26 +5,33 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
     app.get("/", function(req, res) {
         if (req.user) {
-            res.redirect("/members");
+            res.redirect("/landing");
         }
-        res.sendFile(path.join(__dirname, "../public/main.html"))
+        res.sendFile(path.join(__dirname, "../public/index.html"))
     });
 
     app.get("/signup", function(req, res) {
         if (req.user) {
-            res.redirect("/members")
+            res.redirect("/main")
         }
         res.sendFile(path.join(__dirname, "../public/signup.html"))
     });
 
     app.get("/login", function(req, res) {
         if (req.user) {
-            res.redirect("/members")
+            res.redirect("/main")
         }
         res.sendFile(path.join(__dirname, "../public/login.html"))
     });
 
-    app.get("/members", isAuthenticated, function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/members.html"))
+    app.get("/main", function(req, res) {
+        if (req.user) {
+            res.redirect("/main")
+        }
+        res.sendFile(path.join(__dirname, "../public/main.html"))
+    });
+
+    app.get("/main", isAuthenticated, function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/main.html"))
     });
 };
