4d41611520cbbc7a3fffce139b24f8efbfcf4630
Arnold Diep <tungd@uw.edu>
2020-01-09 19:29:29 -0800

    arnold

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,26 +5,26 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = function(app) {
     app.get("/", function(req, res) {
         if (req.user) {
-            res.redirect("/members");
+            res.redirect("/sports");
         }
         res.sendFile(path.join(__dirname, "../public/main.html"))
     });
 
     app.get("/signup", function(req, res) {
         if (req.user) {
-            res.redirect("/members")
+            res.redirect("/sports")
         }
         res.sendFile(path.join(__dirname, "../public/signup.html"))
     });
 
     app.get("/login", function(req, res) {
         if (req.user) {
-            res.redirect("/members")
+            res.redirect("/sports")
         }
         res.sendFile(path.join(__dirname, "../public/login.html"))
     });
 
-    app.get("/members", isAuthenticated, function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/members.html"))
+    app.get("/chat", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/chat.html"))
     });
 };
